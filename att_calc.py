12cdaff7c29a77222d5eaf048bf3a14b5b376a2c
kalk2 <kjellkm83@googlemail.com>
2022-05-25 10:19:08 +0100

    version 2

--- att_calc.py
+++ att_calc.py
@@ -5,7 +5,6 @@ Created on Fri Aug 17 09:59:52 2018
 Description: calculate attenuation and absorption efficiencies
 
 Use: python att_calc.py material thickness(mm) energy(keV)
-
 Output: results output to terminal...
 
 @author: kalkm1
@@ -16,127 +15,27 @@ import pandas as pd
 import sys
 from scipy.interpolate import interp1d
 
+import densities as dens
+
+
+def get_data(material):
+    try:
+        df = pd.read_csv('materials/%s_absorptioncoe_all.csv' % material)
+    except FileNotFoundError:
+        df = np.loadtxt('materials/%s_absorptioncoe_all.txt' % material, skiprows=2)
+        df[:,0] = df[:,0] * 1000 # convert MeV to keV
+        df = pd.DataFrame(df,columns=['keV', 'coherent','incoherent','photoelectric effect','tot(cm2/g)'])
+
+    return df
 
 
 def main(material, x, ephot):
 
-    # material
-    if material == 'CdTe' or material == 'cdte':
+    # get attenuation coefficients
+    df = get_data(material)
 
-        density = 5.85 #g/cm^3
-        df = pd.read_csv('materials/CdTe_absorptioncoe_all.csv') #NIST XCOM
-
-    elif material == 'W' or material == 'w':
-
-        density = 19.3 # g/cm^3
-        df = pd.read_csv('materials/W_absorptioncoe_all.csv') #NIST XCOM
-
-    elif material == 'Al' or material == 'al':
-
-        density = 2.7 # g/cm^3
-        df = pd.read_csv('materials/Al_absorptioncoe_all.csv') #NIST XCOM
-
-    elif material == 'Mo' or material == 'mo':
-
-        density = 10.28 # g/cm^3
-        df = pd.read_csv('materials/Mo_absorptioncoe_all.csv') #NIST XCOM
-
-    elif material == 'Ag' or material == 'ag':
-
-        density = 10.49 # g/cm^3
-        df = pd.read_csv('materials/Ag_absorptioncoe_all.csv') #NIST XCOM
-
-    elif material == 'Cu' or material == 'cu':
-
-        density = 8.96 # g/cm^3
-        df = pd.read_csv('materials/Cu_absorptioncoe_all.csv') #NIST XCOM
-
-    else:
-        if material == 'V' or material == 'v':
-            density = 5.8 # g/cm^3
-            df = np.loadtxt('materials/V_absorptioncoe_all.txt', skiprows=2) # NIST XCOM
-            df[:,0] = df[:,0] * 1000 # convert MeV to keV
-            df = pd.DataFrame(df,columns=['keV', 'coherent','incoherent','photoelectric effect','tot(cm2/g)'])
-
-        if material == 'Zn' or material == 'zn':
-            density = 7.13 # g/cm^3
-            df = np.loadtxt('materials/Zn_absorptioncoe_all.txt', skiprows=2) # NIST XCOM
-            df[:,0] = df[:,0] * 1000 # convert MeV to keV
-            df = pd.DataFrame(df,columns=['keV', 'coherent','incoherent','photoelectric effect','tot(cm2/g)'])
-
-        if material == 'Nb' or material == 'nb':
-            density = 8.6 # g/cm^3
-            df = np.loadtxt('materials/Nb_absorptioncoe_all.txt', skiprows=2) # NIST XCOM
-            df[:,0] = df[:,0] * 1000 # convert MeV to keV
-            df = pd.DataFrame(df,columns=['keV', 'coherent','incoherent','photoelectric effect','tot(cm2/g)'])
-
-        if material == 'Pd' or material == 'pd':
-            density = 11.9 # g/cm^3
-            df = np.loadtxt('materials/Pd_absorptioncoe_all.txt', skiprows=2) # NIST XCOM
-            df[:,0] = df[:,0] * 1000 # convert MeV to keV
-            df = pd.DataFrame(df,columns=['keV', 'coherent','incoherent','photoelectric effect','tot(cm2/g)'])
-
-        if material == 'Ge' or material == 'ge':
-            density = 5.323 # g/cm^3
-            df = np.loadtxt('materials/Ge_absorptioncoe_all.txt', skiprows=2) # NIST XCOM
-            df[:,0] = df[:,0] * 1000 # convert MeV to keV
-            df = pd.DataFrame(df,columns=['keV', 'coherent','incoherent','photoelectric effect','tot(cm2/g)'])
-
-        if material == 'Cr' or material == 'cr':
-            density = 7.19 # g/cm^3
-            df = np.loadtxt('materials/Cr_absorptioncoe_all.txt', skiprows=2) # NIST XCOM
-            df[:,0] = df[:,0] * 1000 # convert MeV to keV
-            df = pd.DataFrame(df,columns=['keV', 'coherent','incoherent','photoelectric effect','tot(cm2/g)'])
-
-        if material == 'Au' or material == 'au':
-            density = 19.32 # g/cm^3
-            df = np.loadtxt('materials/Au_absorptioncoe_all.txt', skiprows=2) # NIST XCOM
-            df[:,0] = df[:,0] * 1000 # convert MeV to keV
-            df = pd.DataFrame(df,columns=['keV', 'coherent','incoherent','photoelectric effect','tot(cm2/g)'])
-
-        if material == 'Mg' or material == 'mg':
-            density = 1.738 # g/cm^3
-            df = np.loadtxt('materials/Mg_absorptioncoe_all.txt', skiprows=2) # NIST XCOM
-            df[:,0] = df[:,0] * 1000 # convert MeV to keV
-            df = pd.DataFrame(df,columns=['keV', 'coherent','incoherent','photoelectric effect','tot(cm2/g)'])
-
-        if material == 'Mn' or material == 'mn':
-            density = 7.43 # g/cm^3
-            df = np.loadtxt('materials/Mn_absorptioncoe_all.txt', skiprows=2) # NIST XCOM
-            df[:,0] = df[:,0] * 1000 # convert MeV to keV
-            df = pd.DataFrame(df,columns=['keV', 'coherent','incoherent','photoelectric effect','tot(cm2/g)'])
-
-        if material == 'CZT' or material == 'czt':
-            density = 5.8 # g/cm^3
-            df = np.loadtxt('materials/CZT_absorptioncoe_all.txt', skiprows=2) # NIST XCOM
-            df[:,0] = df[:,0] * 1000 # convert MeV to keV
-            df = pd.DataFrame(df,columns=['keV', 'coherent','incoherent','photoelectric effect','tot(cm2/g)'])
-
-        if material == 'Pb' or material == 'pb':
-            density = 11.34 # g/cm^3
-            df = np.loadtxt('materials/Pb_absorptioncoe_all.txt', skiprows=2) # NIST XCOM
-            df[:,0] = df[:,0] * 1000 # convert MeV to keV
-            df = pd.DataFrame(df,columns=['keV', 'coherent','incoherent','photoelectric effect','tot(cm2/g)'])
-
-        if material == 'Pt' or material == 'pt':
-            density = 21.45 # g/cm^3
-            df = np.loadtxt('materials/Pt_absorptioncoe_all.txt', skiprows=2) # NIST XCOM
-            df[:,0] = df[:,0] * 1000 # convert MeV to keV
-            df = pd.DataFrame(df,columns=['keV', 'coherent','incoherent','photoelectric effect','tot(cm2/g)'])
-
-        if material == 'Perspex' or material == 'perspex':
-            density = 1.19 # g/cm^3
-            df = np.loadtxt('materials/Perspex_absorptioncoe_all.txt', skiprows=2) # NIST XCOM
-            df[:,0] = df[:,0] * 1000 # convert MeV to keV
-            df = pd.DataFrame(df,columns=['keV', 'coherent','incoherent','photoelectric effect','tot(cm2/g)'])
-
-        if material == 'air' or material == 'air':
-            density = 0.001225 # g/cm^3
-            df = np.loadtxt('materials/air_absorptioncoe_all.txt', skiprows=2) # NIST XCOM
-            df[:,0] = df[:,0] * 1000 # convert MeV to keV
-            df = pd.DataFrame(df,columns=['keV', 'photoelectric effect','tot(cm2/g)'])
-
-
+    # get density of material
+    density = dens.density(material)
 
     # thickness to cm
     ephot = float(ephot)
@@ -173,7 +72,7 @@ def main(material, x, ephot):
 
 
 if __name__ == "__main__":
-    material = (sys.argv[1])
+    material = (sys.argv[1].lower())
     x = (sys.argv[2])
     ephot = (sys.argv[3])
     main(material, x, ephot)
---
+++ densities.py
@@ -0,0 +1,26 @@
+def density(material):
+    '''returns density of material in g/cm^3'''
+
+    den = {'cdte': 5.85,
+           'w': 19.3,
+           'al': 2.7,
+           'mo': 10.28,
+           'ag': 10.49,
+           'cu': 8.96,
+           'v': 5.8,
+           'zn': 7.13,
+           'nb': 8.6,
+           'pd': 11.9,
+           'ge': 5.323,
+           'cr': 7.19,
+           'au': 19.32,
+           'mg': 1.738,
+           'mn': 7.43,
+           'cdznte': 5.8,
+           'pb': 11.34,
+           'pt': 21.45,
+           'perspex': 1.19,
+           'air': 0.001225
+           }
+
+    return den[material]
